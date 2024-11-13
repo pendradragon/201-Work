9fd01991ea4543a54f66006b2095b83236896749
Megan <143452991+pendradragon@users.noreply.github.com>
2024-11-13 13:56:31 -0500

    Add files via upload

---
+++ mal/VulnServer_Fuzzer/buffer_overflow.py
@@ -0,0 +1,27 @@
+import socket
+
+def send_trun_command():
+	target_ip = "192.168.192.153"
+	target_port = 9999
+
+	#connecting to VulnServer
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		try: #trying to connect to vulnserver
+			s.connect(target_ip, target_port)
+			print(f"Connected to {target_ip}:{target_port}")
+
+			#sending the TRUN HELLO command
+			command = "TRUN HELLO\r\n"
+			s.sendall(command.encode())
+			print(f"Command sent: '{command}'")
+
+			#receiving the response
+			response = s.recv(1024)
+			print("Response from server: " + response)
+
+		#error handling, my beloved
+		except ConnectionError as e:
+			print(f"Failed to connect to server or send data: {e}")
+
+if __name__ == "__main__":
+	send_trun_command()
